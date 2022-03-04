9894b743a37195ba48e42e99ade2f077502a90e4
J-oseph <86035099+J-oseph@users.noreply.github.com>
2022-03-03 16:47:45 -0800

    added training data for each speaker, added mel cepstrum, started on average, refactored

--- Code/AGJA_speaker_recognition.m
+++ Code/AGJA_speaker_recognition.m
@@ -1,34 +1,61 @@
-%code
-clc;
+%clc;
 clear;
 close;
+% declare variables
 N = 256;
 M = 100;
 P = 20;
+% import the training data from data files
+[training_audio,fs] = getAudioFiles();
 
-[s1_audio,fs] = audioread('C:\Users\joe\Documents\GitHub\Speaker-Recognition\Speaker-Recognition\Data\Training_Data\s1.wav');
-
-m = melfb(P, N, fs);
+% get the filterband coeff's
+filterbank_coeff = melfb(P, N, fs);
 mel_n = 1 + floor(N/2);
 
-n1 = 1:N-M:length(s1_audio)-N;
-n2 = n1+N;
+% precompute the Hamming window
 window = 0.54-0.46.*cos(2*pi*[0:1:N-1]./(N-1));
-%plot(window)
-for i = 1:1:length(n1)
-    s1_frames{i} = s1_audio(n1(i):n2(i)-1,:); %get the frames
-    s1_frames1{i} = downsample(conv(s1_frames{i},window),2); % window each frame (downsample bc its too long)
-    s1_frames2{i} = abs(fft(s1_frames1{i},N))'; % take the fft
-    for j = 1:1:P
-        z{i,j} = m * abs(s1_frames2{i}(1:mel_n)).^2;
+num = zeros(11,P-1);
+for i = 1:1:length(training_audio)
+    
+    frame_beginning = 1:N-M:length(training_audio{i})-N;
+    frame_end = frame_beginning + N;
+    %plot(window)
+    for j = 1:1:length(frame_beginning)
+        %create the frames
+        frames1{i,j} = training_audio{i}(frame_beginning(j):frame_end(j)-1,:)';
+        % window each frame
+        frames2{i,j} =window.*frames1{i,j};
+        % take FFT and absolute value
+        frames3{i,j} = abs(fft(frames2{i,j},N));
+        % get the mel spectrum coeff (P amount per frame)
+        frames4{i,j} = filterbank_coeff * abs(frames3{i,j}(1:mel_n)).^2';
+        % get mel cepstrum: DCT of the LOG of each spectrum coeff
+        % ignore the first element (P-1 amount per frame)
+        frames5{i,j} = dct(log10(frames4{i,j}));
+        frames5{i,j} = frames5{i,j}(2:end);
+        % get a running total to get average (?)
+        for k = 1:1:P-1
+            num(i,k) = num(i,k) + frames5{i,j}(k);
+        end
     end
+    % calculate average (?)
+    for k = 1:1:P-1
+        num(i,k) = num(i,k) / length(frame_beginning);
+    end
+
 end
-subplot(3,1,1)
-plot(s1_frames{40})
-subplot(3,1,2)
-plot(s1_frames1{40})
-subplot(3,1,3)
-plot(s1_frames2{40})
+    % average
+% subplot(2,1,1)
+% stem(frames2{1,40})
+% subplot(2,1,2)
+% stem(frames3{1,40})
+
+% subplot(3,1,1)
+% plot(frames1{40})
+% subplot(3,1,2)
+% plot(frames2{40})
+% subplot(3,1,3)
+% plot(frames3{40})
 % z = melfb(P, N, fs);
 %  plot(linspace(0, (12500/2), 129), z'),
 %  title('Mel-spaced filterbank'), xlabel('Frequency (Hz)');
---
+++ Code/getAudioFiles.m
@@ -0,0 +1,12 @@
+function [g,fs] = getAudioFiles()
+path = getPath(); 
+% path is specific for each computer
+% should be something like 'C:\....\Speaker-Recognition'
+subpath = '\Data\Training_Data\s';
+ext = '.wav';
+[audio,fs] = audioread(strcat(path,subpath,int2str(1),ext));
+g = {audio(:,1)};
+for index = 2:1:11
+    [audio,fs] = audioread(strcat(path,subpath,int2str(index),ext));
+    g{end+1}=(audio(:,1));
+end
