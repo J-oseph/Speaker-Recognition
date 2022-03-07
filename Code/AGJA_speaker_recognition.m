8e2707d702f1c92d29d24c82e2d39465a3d4377f
J-oseph <86035099+J-oseph@users.noreply.github.com>
2022-03-07 13:23:44 -0800

    simplified getting files, refactored, gitignore
    combined getAudioFiles and getTestAudioFiles into one function. ignored Mac files, small tweaks on main

--- Code/AGJA_speaker_recognition.m
+++ Code/AGJA_speaker_recognition.m
@@ -1,12 +1,14 @@
 clc;
 clear;
 close;
+
 % declare variables
 N = 256;
 M = 100;
 P = 20;
+
 % import the training data from data files
-[training_audio,fs] = getAudioFiles();
+[training_audio,fs] = getAudioFiles('train');
 
 %normalize audio signals
 for i = 1:length(training_audio)
@@ -19,21 +21,23 @@ mel_n = 1 + floor(N/2);
 
 % precompute the Hamming window
 window = 0.54-0.46.*cos(2*pi*[0:1:N-1]./(N-1));
+
 for i = 1:1:length(training_audio)
-     
+    % i is the index of each speaker
+    % calculate frame beg, end, and amount
     frame_beginning = 1:M:length(training_audio{i})-N;
-    frame_end = frame_beginning + N;
-    %plot(window)
-    frame_length(i) = length(frame_beginning);
-    for j = 1:1:length(frame_beginning)
-       %create the frames
+    frame_end       = frame_beginning + N;
+    frame_amount(i) = length(frame_beginning);
+
+    for j = 1:1:frame_amount(i)
+        %create the frames
         frames1{i,j} = training_audio_norm{i}(frame_beginning(j):frame_end(j)-1,:)';
         % window each frame
-        frames2{i,j} =window.*frames1{i,j};
+        frames2{i,j} = window.*frames1{i,j};
         % take FFT and absolute value
         frames3{i,j} = abs(fft(frames2{i,j},N));
         % get the mel spectrum coeff (P amount per frame)
-        frames4{i,j} = filterbank_coeff * abs(frames3{i,j}(1:mel_n)).^2';%check .^2
+        frames4{i,j} = filterbank_coeff * abs(frames3{i,j}(1:mel_n)).^2'; %check .^2
         % get mel cepstrum: DCT of the LOG of each spectrum coeff
         frames5{i,j} = dct(log10(frames4{i,j}));
         % ignore the first element (P-1 amount per frame)
@@ -41,7 +45,7 @@ for i = 1:1:length(training_audio)
     end
 end
 
-[test_audio, fs] = getTestAudioFiles();
+[test_audio, fs] = getAudioFiles('test');
 
 %normalization step?
 %abs
@@ -65,7 +69,7 @@ i=1;
     % each column is a speaker, each row is sum of mel coeffs
     row_sum(:,i) = sum(row_mat,2);
     % divide by number of frames
-    row_avg(:,i) = row_sum(:,i)/frame_length(i); 
+    row_avg(:,i) = row_sum(:,i)/frame_amount(i); 
     %define centroid matrix
     centroids(:,:,i) = row_avg(:,i);
     while size(centroids,2) < codeword_lim
@@ -116,21 +120,7 @@ i = 1;%test on speaker one
     
 
 
-%sound(training_audio{8})
-%sound(test_audio{8})
 
-    % average
-% subplot(2,1,1)
-% stem(frames2{1,40})
-% subplot(2,1,2)
-% stem(frames3{1,40})
-
-% subplot(3,1,1)
-% plot(frames1{40})
-% subplot(3,1,2)
-% plot(frames2{40})
-% subplot(3,1,3)
-% plot(frames3{40})
 % z = melfb(P, N, fs);
 %  plot(linspace(0, (12500/2), 129), z'),
 %  title('Mel-spaced filterbank'), xlabel('Frequency (Hz)');
--- Code/getAudioFiles.m
+++ Code/getAudioFiles.m
@@ -1,12 +1,20 @@
-function [g,fs] = getAudioFiles()
+function [g,fs] = getAudioFiles(type)
 path = getPath();
-% path is specific for each computer
+% path is specific for each computer. Needs to go to the Repo folder
 % should be something like 'C:\....\Speaker-Recognition'
-subpath = '/Data/Training_Data/s';
+N = 1;
+subpath = '';
 ext = '.wav';
+if (strcmp(type,'test'))
+    subpath = '/Data/Test_Data/s';
+    N = 8;
+elseif (strcmp(type,'train'))
+    subpath = '/Data/Training_Data/s';
+    N = 11;
+end
 [audio,fs] = audioread(strcat(path,subpath,int2str(1),ext));
 g = {audio(:,1)};
-for index = 2:1:11
+for index = 2:1:N
     [audio,fs] = audioread(strcat(path,subpath,int2str(index),ext));
     g{end+1}=(audio(:,1));
 end
