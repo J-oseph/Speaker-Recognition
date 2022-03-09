98c702f72b321a8078607ce11016952838265f59
ansongrover <ansongrover7525@gmail.com>
2022-03-08 16:27:09 -0800

    Added code for testing algorithm

--- Code/AGJA_speaker_recognition.m
+++ Code/AGJA_speaker_recognition.m
@@ -2,6 +2,7 @@
 clear;
 close;
 
+%% Get mel coeffs for training data
 % declare variables
 N = 256;
 M = 100;
@@ -46,8 +47,9 @@ for i = 1:1:length(training_audio)
     end
 end
 
-[test_audio, fs] = getAudioFiles('test');
 
+
+%% LBG
 
 %Parameters
 %it's tough to come up with good error metric since a "low" total
@@ -55,14 +57,11 @@ end
 %i've based the error on how much it changes between iterations
 thresh = .001; %thresh*100 is allowed error percent change 
 epsilon = .01; %splitting parameter
-codeword_lim = 16; %maximum number of centroids
+codeword_lim = 32; %maximum number of centroids
 
-%make code books
 for i = 1:length(training_audio) %loop thru speakers
-    %take all mel coeffs of all frames of ith speaker as cell array
-    temp_row = frames5(i,:); 
     % convert to matrix, index 1 is mel coeff coordinate, index 2 is frame
-    row_mat = cell2mat(temp_row); 
+    row_mat = cell2mat(frames5(i,:)); 
     % column vector, each entry is sum of mel coeffs
     row_sum = sum(row_mat,2);
     % divide by number of frames
@@ -87,7 +86,7 @@ for i = 1:length(training_audio) %loop thru speakers
             
         %compute distance of each frame vector to centroids and determine closer centroid     
         d = disteu(row_mat,centroids{i}); %d has centroid# 2nd index
-        [~,I] = min(d,[],2);
+        [~,I] = min(d,[],2); 
         
         
         %initialize cell array to store frames after centroid splitting
@@ -115,27 +114,73 @@ for i = 1:length(training_audio) %loop thru speakers
         
         %find average distance
         d = disteu(row_mat,centroids{i});
-        [~,I] = min(d,[],2);
         err_new = sum(min(d,[],2));     
         end
     end
 end
+
 %Key output is centroids{i} indexed by speaker, each entry is 
 %a 16 x 19 set of centroids which represents the 19 coordinates of each of
 %16 codewords for each speaker
-    
 
-%LBG
+
+%% Get mel coeffs for test audio
+
+[test_audio, fs] = getAudioFiles('test');
+%normalize audio signals - this may not be best way to normalize
+for i = 1:length(test_audio)
+    test_audio_norm{i} = test_audio{i}/max(test_audio{i});
+end
+for i = 1:1:length(test_audio)
+    % i is the index of each speaker
+    % calculate frame beg, end, and amount
+    frame_beginning = 1:M:length(test_audio{i})-N;
+    frame_end       = frame_beginning + N;
+    frame_amount(i) = length(frame_beginning);
+
+    for j = 1:1:frame_amount(i)
+        %create the frames
+        framest1{i,j} = test_audio_norm{i}(frame_beginning(j):frame_end(j)-1,:)';
+        % window each frame
+        framest2{i,j} = window.*framest1{i,j};
+        % take FFT and absolute value
+        framest3{i,j} = abs(fft(framest2{i,j},N));
+        % get the mel spectrum coeff (P amount per frame)
+        framest4{i,j} = filterbank_coeff * abs(framest3{i,j}(1:mel_n)).^2'; %check .^2
+        % get mel cepstrum: DCT of the LOG of each spectrum coeff
+        framest5{i,j} = dct(log10(framest4{i,j})); 
+        % ignore the first element (P-1 amount per frame)
+        framest5{i,j} = framest5{i,j}(2:end);
+    end
+end
+
+
+%% Compare test audio to training audio
+
+ for j = 1:8; %pick which test audio file 1-8
+     row_mat = cell2mat(framest5(j,:)); %mel coeffs for speaker i converted to array
+     for i = 1:length(training_audio) %loop thru training set
+        d = disteu(row_mat,centroids{i});
+        distortion(i) = sum(min(d,[],2));
+     end
+     [~,I] = min(distortion);
+     disp(I) %displays which training audio that corresponds to
+ end
+ 
+%sound(training_audio{3})
+%sound(test_audio{3})
+
+    
+%LBG summary
 %sum over each mel coeff across all frames(~140-270) of
 %each speaker, divide the total by the number of frames, this vector is the
 %cetroid of all data for that speaker, then take each vector component and
 %multiply it by 1+epsilon, and then take each component and multiply it by
-%1-epsilon, compute the distance between each vector and each centroid,
+%1-epsilon, compute the distance between each vector and each new centroid,
 %assign each vector to the closer centroid, recompute the centroids by
 %finding the centroid of all the vectors assigned to it, repeat process
 %until below threshold,once happy with error, split centroids again by 
 %multiplying each by 1+epsilon, and 1-epsilon
-
 
 
 % z = melfb(P, N, fs);
